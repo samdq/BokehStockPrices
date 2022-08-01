a021ca923326ca20979eb2d0889ab590c0d143b0
Linus XCJ <70716538+linusxcj@users.noreply.github.com>
2022-08-01 16:03:26 +0530

    update file

--- interactive_charts.py
+++ interactive_charts.py
@@ -8,7 +8,10 @@ def run_interactive_charts_example():
     stock_symbol = "AAPL"
     stock_data = fetch_stock_data(stock_symbol, interval="1d", output_size="compact")
 
-
+    if stock_data is not None:
+        # Create a Bokeh figure
+        p = figure(title=f"{stock_symbol} Stock Price", x_axis_label="Date", y_axis_label="Closing Price",
+                   x_axis_type="datetime", sizing_mode="stretch_width", toolbar_location="above")
 
         # Plot the closing price as a line
         p.line(stock_data.index, stock_data["4. close"], line_width=2, legend_label="Closing Price", color="blue")
