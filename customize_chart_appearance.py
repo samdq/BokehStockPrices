7ae9fd572c21bc8b499b8121b4529fec8335ced9
Linus XCJ <70716538+linusxcj@users.noreply.github.com>
2023-01-12 19:55:15 +0530

    update file

--- customize_chart_appearance.py
+++ customize_chart_appearance.py
@@ -15,14 +15,6 @@ def run_customize_chart_appearance_example():
     stock_symbol = "AAPL"
     stock_data = fetch_stock_data(stock_symbol, interval="1d", output_size="compact")
 
-    if stock_data is not None:
-        # Create a Bokeh figure
-        p = figure(title=f"{stock_symbol} Stock Price with Custom Appearance", x_axis_label="Date",
-                   y_axis_label="Closing Price", x_axis_type="datetime", sizing_mode="stretch_width",
-                   toolbar_location="above")
-
-        # Plot the closing price as a line
-        p.line(stock_data.index, stock_data["4. close"], line_width=2, legend_label="Closing Price", color="blue")
 
         # Add HoverTool for displaying additional information on hover
         hover = HoverTool()
