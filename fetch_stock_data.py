3be7df4d11d0d20074a21ef79bef9be1371550ef
Linus XCJ <70716538+linusxcj@users.noreply.github.com>
2023-01-25 20:01:38 +0530

    update file

--- fetch_stock_data.py
+++ fetch_stock_data.py
@@ -7,7 +7,35 @@ import pandas as pd
 from bokeh_stock_prices.config import ALPHA_VANTAGE_API_KEY
 
 # Function to fetch historical stock data from Alpha Vantage API
+def fetch_stock_data(symbol, interval="1d", output_size="compact"):
+    try:
+        # API endpoint and parameters
+        api_url = "https://www.alphavantage.co/query"
+        function = "TIME_SERIES_INTRADAY" if interval == "1d" else "TIME_SERIES_DAILY"
+        params = {
+            "function": function,
+            "symbol": symbol,
+            "interval": interval,
+            "apikey": ALPHA_VANTAGE_API_KEY,
+            "outputsize": output_size,
+        }
+        # Make API request
+        response = requests.get(api_url, params=params)
+        response.raise_for_status()
 
+        # Parse response JSON into DataFrame
+        data = response.json()
+        if "Time Series (Daily)" in data:
+            df = pd.DataFrame(data["Time Series (Daily)"]).T
+            df.index = pd.to_datetime(df.index)
+            df.sort_index(inplace=True)
+            return df
+        else:
+            print(f"Error: {data['Note']}")
+            return None
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching stock data for {symbol}: {e}")
+        return None
 
 # Example Usage:
 # stock_symbol = "AAPL"
