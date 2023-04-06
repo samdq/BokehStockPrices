cd7d530e6b0c862fc867ed9a1bf6ce631481a194
Linus XCJ <70716538+linusxcj@users.noreply.github.com>
2023-04-06 20:03:41 +0530

    update file

--- fetch_stock_data.py
+++ fetch_stock_data.py
@@ -1,6 +1,10 @@
 # bokeh_stock_prices/fetch_stock_data.py
 # Logic to fetch historical stock data from the Alpha Vantage API
 
+import os
+import requests
+import pandas as pd
+from bokeh_stock_prices.config import ALPHA_VANTAGE_API_KEY
 
 # Function to fetch historical stock data from Alpha Vantage API
 def fetch_stock_data(symbol, interval="1d", output_size="compact"):
