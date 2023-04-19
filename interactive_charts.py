62869c1aea286fa34e66651a76d2195c644d6726
Linus XCJ <70716538+linusxcj@users.noreply.github.com>
2023-04-19 20:06:19 +0530

    update file

--- interactive_charts.py
+++ interactive_charts.py
@@ -3,11 +3,6 @@
 
 
 
-# Function to run the interactive charts example
-def run_interactive_charts_example():
-    stock_symbol = "AAPL"
-    stock_data = fetch_stock_data(stock_symbol, interval="1d", output_size="compact")
-
     if stock_data is not None:
         # Create a Bokeh figure
         p = figure(title=f"{stock_symbol} Stock Price", x_axis_label="Date", y_axis_label="Closing Price",
