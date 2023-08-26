69b05e87ed177b4c3a83219645c3218ec61a1425
Linus XCJ <70716538+linusxcj@users.noreply.github.com>
2023-08-26 15:56:10 +0530

    update file

--- __init__.py
+++ __init__.py
@@ -1,14 +1,3 @@
 import os
 
-# Ensure that the 'data' directory exists
-DATA_DIR = os.path.join(os.path.dirname(__file__), "data")
-os.makedirs(DATA_DIR, exist_ok=True)
-
-# Ensure that the 'templates' directory exists
-TEMPLATES_DIR = os.path.join(os.path.dirname(__file__), "templates")
-os.makedirs(TEMPLATES_DIR, exist_ok=True)
-
-# Ensure that the 'static' directory exists
-STATIC_DIR = os.path.join(os.path.dirname(__file__), "static")
-os.makedirs(STATIC_DIR, exist_ok=True)
 
